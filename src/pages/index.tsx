b15d8699ac43a75183a4207db0baa0a332b549b0
erickmarquesc <erick.marquesc@hotmail.com>
2022-05-16 22:05:36 -0300

    feat: :tada: init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,68 @@
+import { Button, Flex, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Flex
+      w='100vw'
+      h='100vh'
+      align='center'
+      justify='center'
+    >
+      <Flex
+        as='form'
+        w='100%'
+        maxW={360} //PIXEL
+        bg='gray.800'
+        p='8' // Isso é igual a 2 rem pois 8/4=2rem ou 8x4=32px
+        borderRadius={8}
+        flexDir='column' // direção do flez em coluna
+      >
+        <Stack spacing='4'>
+
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name='email'
+              id='email'
+              type='email'
+              focusBorderColor="pink.500"
+              bgColor='gray.900'
+              variant='filled' // tipo do imput
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size='lg'
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              name='password'
+              id='password'
+              type='password'
+              focusBorderColor="pink.500"
+              bgColor='gray.900'
+              variant='filled'
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size='lg'
+
+            />
+          </FormControl>
+
+        </Stack>
+        <Button
+          type="submit"
+          mt='6'
+          colorScheme='pink'
+          size='lg'
+        >
+          Entrar
+        </Button>
+
+      </Flex>
+    </Flex>
+  )
+}
