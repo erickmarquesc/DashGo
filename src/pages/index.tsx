2dfcae25fbd0ce3d4da7450a22239f330ba75b37
erickmarquesc <erick.marquesc@hotmail.com>
2022-05-18 00:47:50 -0300

    refactor: :fire: created components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Button, Flex, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Input";
 
-export default function Home() {
+export default function SigIn() {
   return (
     <Flex
       w='100vw'
@@ -19,38 +20,8 @@ export default function Home() {
       >
         <Stack spacing='4'>
 
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input
-              name='email'
-              id='email'
-              type='email'
-              focusBorderColor="pink.500"
-              bgColor='gray.900'
-              variant='filled' // tipo do imput
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size='lg'
-            />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input
-              name='password'
-              id='password'
-              type='password'
-              focusBorderColor="pink.500"
-              bgColor='gray.900'
-              variant='filled'
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size='lg'
-
-            />
-          </FormControl>
+          <Input name="email" type="email" label="E-mail"/>
+          <Input name="password" type="password" label="Password"/>
 
         </Stack>
         <Button
@@ -64,5 +35,5 @@ export default function Home() {
 
       </Flex>
     </Flex>
-  )
+  );
 }
