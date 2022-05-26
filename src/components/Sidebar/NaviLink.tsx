5a2c8abffa5d64674feecb027b641f8634d60106
erickmarquesc <erick.marquesc@hotmail.com>
2022-05-26 14:39:48 -0300

    feat: :sparkles: add component sidebar

--- src/components/Sidebar/NaviLink.tsx
+++ src/components/Sidebar/NaviLink.tsx
@@ -1,15 +1,19 @@
-import { Icon, LinkProps, Text, Link } from "@chakra-ui/react";
+import { Icon, LinkProps, Text, Link as ChakraLink } from "@chakra-ui/react";
+import { ActiveLink } from "./ActiveLink";
 import { ElementType } from "react";
 
-interface INaviLinkProps extends LinkProps{
+interface INaviLinkProps extends LinkProps {
   icon: ElementType;
   children: string;
+  href: string;
 }
-export function NavLink({ icon, children, ...rest }: INaviLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: INaviLinkProps) {
   return (
-    <Link display="flex" alignItems="center" {... rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">{children}</Text>
-    </Link>
+    <ActiveLink href={href} passHref>
+      <ChakraLink display="flex" alignItems="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">{children}</Text>
+      </ChakraLink>
+    </ActiveLink>
   );
 }
