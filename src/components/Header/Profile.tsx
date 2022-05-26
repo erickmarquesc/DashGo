fad72c091d55bc524443436cb0c391df4e48faa8
erickmarquesc <erick.marquesc@hotmail.com>
2022-05-26 14:38:58 -0300

    refactor: :recycle: refactor components header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,17 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface IProfileProps{
+  showProfileData: boolean;
+}
+export function Profile({showProfileData}: IProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
         <Text>Erick Marques</Text>
         <Text color="gray.300" fontSize="small">erick.marquesc@hotmail.com</Text>
       </Box>
+      )}
       <Avatar size="md" name="Erick Marques" src="https://avatars.githubusercontent.com/u/56804790?v=4" />
     </Flex>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
-import { Flex } from "@chakra-ui/react";
-import { Logo } from "./Logo";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 import { NotificationsNav } from "./NotificationsNav";
-import { Profile } from "./Profile";
+import { RiMenuLine } from "react-icons/ri";
 import { SearchBox } from "./SearchBox";
+import { Profile } from "./Profile";
+import { Logo } from "./Logo";
 
-export function Header() {
+export function Header() {/* Compoenente Header */
+
+  const { onOpen } = useSidebarDrawer(); /* onOpen é um metodo que seta como true o isOpen */
+
+  const isWideVersion = useBreakpointValue({/* Hook que ajuda na responsividade das telas */
+    base: false, /* Quando estamos em uma tela menor que lg */
+    lg: true, /* Por padrão estamos sempre na versão de tela maior  */
+  });
+
   return (
     <Flex
       as="header"
@@ -15,23 +25,41 @@ export function Header() {
       mt="4"
       px="6"
       align="center"
+      justifyContent="space-between"
     >
+      {/* Botão de navegação mobile */}
+      {/* Por padrão o isWideVersion será TRUE pois estamos em uma tela >= que lg */}
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navegation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        >
+
+        </IconButton>
+      )}
+
       {/* LOGO */}
       <Logo />
 
+
       {/* PESQUISA */}
-      <SearchBox />
+      {/* A barra só aparece caso a tela seja >= a lg | Por padrão ela aparece */}
+      {isWideVersion && <SearchBox />}
 
       {/* USER */}
-      <Flex
-        align="center"
-        ml="auto"
-      >
+      <Flex align="center" ml="auto">
+
         {/* NOTIFICAÇÕES */}
         <NotificationsNav />
+        
+        {/* Perfil */}
+        <Profile showProfileData={isWideVersion} />
 
-        <Profile />
       </Flex>
-    </Flex>
+    </Flex >
   );
 }
