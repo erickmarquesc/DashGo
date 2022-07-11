efb7b5d4e7a16296cda091cb59581b9411aebca0
erickmarquesc <erick.marquesc@hotmail.com>
2022-07-11 20:11:54 -0300

    feat: :art: format of the code TableHeader

--- src/components/TableUser/TableHeader.tsx
+++ src/components/TableUser/TableHeader.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Divider, Box } from "@chakra-ui/react";
+import { Flex, Heading, Divider, Box, Spinner } from "@chakra-ui/react";
 import { RiAddLine } from "react-icons/ri";
 import { CTAButton } from "../CTAButton";
 import Link from "next/link";
@@ -6,15 +6,20 @@ import Link from "next/link";
 interface ITableHeaderProps {
   title: string,
   button?: boolean;
+  fetching?: boolean;
+  loading?: boolean;
 };
 
-export function TableHeader({ title, button = false }: ITableHeaderProps) {
+export function TableHeader({ title, button = false, fetching, loading }: ITableHeaderProps) {
   return (
     <Box>
 
       <Flex mb="8" justifyContent="space-between" align="center">
 
-        <Heading size="lg" fontWeight="normal">{title}</Heading>
+        <Heading size="lg" fontWeight="normal">
+          {title}
+          {!loading && fetching && <Spinner size='sm' color='pink.500' ml='4'/>}
+        </Heading>
 
         {button && (
           <Link href="/users/create" passHref>
@@ -23,7 +28,7 @@ export function TableHeader({ title, button = false }: ITableHeaderProps) {
         )}
       </Flex>
       <Divider my="6" borderColor="gray.700" />
-      
+
     </Box>
   );
 };
