6d1518ae7dbcfb6db6ce88551b89c1d4e27d78ba
erickmarquesc <erick.marquesc@hotmail.com>
2022-07-11 20:08:25 -0300

    feat: add mirage

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,5 +1,5 @@
-import { createServer, Factory, Model } from "miragejs";
-import {faker} from '@faker-js/faker';
+import { createServer, Factory, Model, Response } from "miragejs";
+import { faker } from '@faker-js/faker';
 
 type User = {
   name: string,
@@ -18,24 +18,40 @@ export function makeServer() {
         name(i: number) {
           return `Usuário ${i + 1}`
         },
-        email() { 
+        email() {
           return faker.internet.email().toLowerCase();
         },
-        createdAt() { 
+        createdAt() {
           return faker.date.recent(10);
         },
       })
     },
 
-    seeds(server){
-      server.createList('user', 30);
+    seeds(server) {
+      server.createList('user', 60);
     },
 
     routes() { // Shorthands do mirage para controlar rotas
       this.namespace = 'api';
       this.timing = 750;
 
-      this.get('/users');
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all('user').length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all('user')).users.slice(pageStart, pageEnd);
+
+        return new Response(
+          200,
+          { 'x-total-count': String(total) },
+          { users }
+        );
+      });
+
       this.post('/users');
 
       this.namespace = '';
