ba2e4f0bdcc924000a53bf897dcdfbd1f2f8f1f5
erickmarquesc <erick.marquesc@hotmail.com>
2022-07-11 21:36:24 -0300

    feat: :white_check_mark: update pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,11 +1,42 @@
 import { PaginationItem } from "./PaginationItem";
-import { Box, Stack } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/react";
 
-export function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+
+}
+
+const siblingsCount = 1;
+
+/* PÁGINAS POSSÍVEIS AO LADO */
+function generatePagesArray(from: number, to: number) {
+  return [... new Array(to - from)].map((_, index) => {
+    return from + index + 1;
+  }).filter(page => page > 0);
+};
+
+export function Pagination(
+  { totalCountOfRegisters, registersPerPage = 10, currentPage = 1 }: PaginationProps) {
+
+  /* ULTIMA PÁGINA POSSÍVEL */
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+  /* PÁGINAS POSSÍVEIS ANTERIOR */
+  const previousPages = currentPage > 1
+    ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+    : []
+
+  /* PÁGINAS POSSÍVEIS A SEGUIR */
+  const nextPages = currentPage < lastPage
+    ? generatePagesArray(currentPage, Math.min(currentPage + siblingsCount, lastPage))
+    : []
+
   return (
     <Stack
       spacing="6"
-      direction={["column","row"]}
+      direction={["column", "row"]}
       mt="8"
       justify="space-between"
       align="center">
@@ -13,11 +44,46 @@ export function Pagination() {
       <Box>
         <strong>0</strong> - <strong>5</strong> de <strong>100</strong>
       </Box>
-      
+
       <Stack direction="row" spacing="2">
-        <PaginationItem number={1} isCurrent />
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
+        {/* Caso tenha mais de 2 páginas anteriores */}
+        {currentPage > (siblingsCount + 1) &&
+          <>
+            <PaginationItem number={1} />
+            {currentPage > (siblingsCount + 2) && (
+              <Text color={'pink.300'} width="8" textAlign="center">...</Text>
+            )
+            }
+          </>
+        }
+
+        {/* Mostra a página anterior */}
+        {previousPages.length > 0
+          && previousPages.map(page => {
+            return <PaginationItem key={page} number={page} />
+          })
+        }
+
+        {/* Página atual */}
+        <PaginationItem number={currentPage} isCurrent />
+
+        {/* Ultima página */}
+        {nextPages.length > 0
+          && nextPages.map(page => {
+            return <PaginationItem key={page} number={page} />
+          })
+        }
+        
+        {/* Caso tenha mais de 2 páginas seguintes */}
+        {(currentPage + siblingsCount) < lastPage &&
+          <>
+            {(currentPage + siblingsCount + 1) < lastPage && (
+              <Text color={'pink.300'} width="8" textAlign="center">...</Text>
+            )
+            }
+            <PaginationItem number={lastPage} />
+          </>
+        }
       </Stack>
     </Stack>
   );
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,9 +22,9 @@ export default function UserList() {
         <Sidebar />
 
         <Box bg="gray.800" flex="1" borderRadius={8} p="8">
-          
+
           <TableHeader title="Usuários" fetching={isFetching} loading={isLoading} button={true} />
-          
+
           <Flex m={["0", "8"]} justifyContent="flex-start" align="center">
 
             <CTAButton m={["2", "8"]} icon={RiCheckboxMultipleFill}>Selecionar todos</CTAButton>
@@ -42,7 +42,9 @@ export default function UserList() {
           ) : (
             <>
               <TableUser />
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={5} />
             </>
           )}
 
